7ce16dd8449507ee0df6dc44005754c462b48528
KH-WebDesigns <10818122@my.uvu.edu>
2019-08-23 22:15:56 -0600

    Adding Unit 01

---
+++ Unit01/assets/js/main.js
@@ -0,0 +1,9 @@
+let companyName = document.getElementById('companyName');
+let companySlogan = document.getElementById('companySlogan');
+let dateTime = Date();
+let displayDate = document.getElementById('displayDate');
+
+
+companyName.innerHTML = "Joe's Bed and Breakfast";
+companySlogan.innerHTML = "Keeping You Warm and Fed";
+displayDate.innerHTML = dateTime;
