536aad81b3573c13237653c17a246f00de051cf2
KH-WebDesigns <10818122@my.uvu.edu>
2019-08-24 07:52:52 -0600

    Updating styles and main.js in Unit01

--- Unit01/assets/js/main.js
+++ Unit01/assets/js/main.js
@@ -2,8 +2,11 @@ let companyName = document.getElementById('companyName');
 let companySlogan = document.getElementById('companySlogan');
 let dateTime = Date();
 let displayDate = document.getElementById('displayDate');
+let welcomeMsg = document.getElementById('welcomeMsg');
+let entryName = window.prompt("Please enter your name!");
 
 
 companyName.innerHTML = "Joe's Bed and Breakfast";
 companySlogan.innerHTML = "Keeping You Warm and Fed";
 displayDate.innerHTML = dateTime;
+welcomeMsg.innerHTML = "Welcome " + entryName + ", to Joe's Bed and Breakfast!";
