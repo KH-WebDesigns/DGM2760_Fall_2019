1de314f5b67423c8ad35917b564457da2a983de0
KH-WebDesigns <10818122@my.uvu.edu>
2019-09-21 11:04:38 -0600

    Adding Unit 05

--- Unit05/assets/js/main.js
+++ Unit05/assets/js/main.js
@@ -1,2 +1,77 @@
 document.querySelector('#headerName').textContent = `The Arborist`
 document.querySelector('#headerSlogan').textContent = `Manipulating Arrays With Events`
+const RESULTS = document.querySelector('#results');
+const ERROR = document.querySelector('#errReport');
+
+
+//================= Initial Tree List =====================//
+
+const TREES = ['oak', 'Spruce', 'Mahogany', 'Redwood'];
+
+    //Displaying this tree list in it's own list
+    const LISTTREES = () => {
+        let treeList = '';
+        TREES.forEach(tree => {
+
+            // console.log(tree);
+            treeList += `${tree} <br>`
+
+        })
+        RESULTS.innerHTML = `${treeList} <span>There are ${TREES.length} Trees On This List</span>`;
+    }
+    
+    LISTTREES();
+
+//============== end Initial tree list ======================//
+
+//Add to the front of the array
+document.querySelector('#addApple').onclick = () => {
+    TREES.unshift('Apple');
+    LISTTREES();
+}
+
+//Add to the end of the array
+document.querySelector('#addSpruce').onclick = () => {
+    TREES.push('Spruce');
+    LISTTREES();
+}
+
+//Get rid of the first tree
+document.querySelector('#deleteFirst').onclick = () => {
+    TREES.shift();
+    LISTTREES();
+}
+
+//Get rid of the second tree
+document.querySelector('#deleteSecond').onclick = () => {
+    TREES.splice(1, 1);
+    LISTTREES();
+}
+
+//Get rid of the last tree
+document.querySelector('#deleteLast').onclick = () => {
+    TREES.pop();
+    LISTTREES();
+}
+
+//Sort Alphabetically
+document.querySelector('#alphaSort').onclick = () => {
+    TREES.sort();
+    LISTTREES();
+}
+
+document.querySelector('#lowercase').onclick = () => {
+    
+    const LISTTREES = () => {
+        let lowerList = '';
+        TREES.forEach(tree => {
+
+            // console.log(tree);
+            lowerList += `${tree} <br>`
+
+        })
+        RESULTS.innerHTML = `${treeList} <span>There are ${TREES.length} Trees On This List</span>`;
+    }
+    
+    LISTTREES();
+}
