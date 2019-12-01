4f59391675ee50ef10be6ee646ec0fe45635cde1
Kurt Heimbigner <10818122@my.uvu.edu>
2019-11-30 19:53:22 -0700

    Editing main.js
    Adding .value to querySelector items to obtain a string from the textbox and converting to an array

--- Unit06/assets/js/main.js
+++ Unit06/assets/js/main.js
@@ -1,10 +1,16 @@
-nounText = document.querySelector('#txtNoun');
-adjText = document.querySelector('#txtAdj');
-verbText = document.querySelector('#txtVerb');
-let nounArray = '';
-let adjArray = '';
-let verbArray ='';
+//grabbing text from the three textarea boxes
+nounText = document.querySelector('#txtNoun').value;
+adjText = document.querySelector('#txtAdj').value;
+verbText = document.querySelector('#txtVerb').value;
+
+/* ensuring that each line of text reads as a separate array elemment then turning that into an array */
+let nounArray = nounText.replace(/^\s+|\s+$/g, '').split(/\s+/);
+let adjArray = adjText.replace(/^\s+|\s+$/g, '').split(/\s+/);
+let verbArray = verbText.replace(/^\s+|\s+$/g, '').split(/\s+/);
+
+//console.logging to prove it worked! (Arrays should be showing)
+console.log(nounArray);
+console.log(adjArray);
+console.log(verbArray);
 
 
-
-console.log(nounText);
