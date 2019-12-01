aa794f4ad28e49df6e3b18219e3e306853856994
Kurt Heimbigner <10818122@my.uvu.edu>
2019-11-30 19:42:56 -0700

    Adding textareas and input support

---
+++ Unit06/assets/js/main.js
@@ -0,0 +1,10 @@
+nounText = document.querySelector('#txtNoun');
+adjText = document.querySelector('#txtAdj');
+verbText = document.querySelector('#txtVerb');
+let nounArray = '';
+let adjArray = '';
+let verbArray ='';
+
+
+
+console.log(nounText);
