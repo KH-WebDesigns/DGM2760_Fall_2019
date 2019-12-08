dff97990ed27f8e800775413ab472ae0248e4346
Kurt Heimbigner <10818122@my.uvu.edu>
2019-12-07 19:40:46 -0700

    Adding shopping list

--- Unit07/assets/js/main.js
+++ Unit07/assets/js/main.js
@@ -4,17 +4,29 @@ const pizza = {
     crust:'Original',
     cheese: 'Regular',
     toppings: 'Pepperoni',
-    createPizza: function() {
+    createPizza: () => {
         console.log('buildPizza method has been called')
         message = `Kurt is now baking your pizza on a ${pizza.crust} crust with ${pizza.cheese} cheese and ${pizza.toppings} on top!`
         document.querySelector('#message').textContent = message
+    },
+    shoppingList: () => {
+        console.log('it worked!')
+        let flour = 1
+        let cheese = 1
+        if (pizza.crust === 'thin') flour /= 2
+        if (pizza.crust === 'thick') flour *= 2 
+        if (pizza.cheese === 'no cheese') cheese = 0
+        if (pizza.cheese === 'extra') cheese *= 2 
+        
+        message = ` You will need to purchase  ${flour} cups of flour, ${cheese}lbs of cheese and 2lbs of ${pizza.toppings}.`
+        document.querySelector('#message').textContent += message
     }
 }
 
 //CRUST radio buttons
 let thinButton = document.querySelector('#thin').addEventListener('click', () => pizza.crust = 'thin')
 document.querySelector('#original').addEventListener('click', () => pizza.crust = 'original')
-document.querySelector('#glutenFree').addEventListener('click', () => pizza.crust = 'gluten free')
+document.querySelector('#thick').addEventListener('click', () => pizza.crust = 'thick')
 
 //CHEESE radio buttons
 document.querySelector('#none').addEventListener('click', () => pizza.cheese = 'no cheese')
@@ -29,4 +41,5 @@ document.querySelector('#supreme').addEventListener('click', () => pizza.topping
 //When the 'Create My Pizza' button is pressed, the ingredients of the pizza will be displayed
 document.querySelector('#createPizza').addEventListener('click', () => pizza.createPizza());
 
-
+//When the 'Create Shopping List' button is pressed, the needed ingredients and their amounts will be displayed
+document.querySelector('#shoppingList').addEventListener('click', () => pizza.shoppingList());
