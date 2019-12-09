7a14b08c378775c40792bd9070a50a21cc8d8441
Kurt heimbigner <10818122@my.uvu.edu>
2019-12-08 19:47:26 -0700

    .

---
+++ Unit11/assets/js/main.js
@@ -0,0 +1,34 @@
+async function getHotelData() {
+    try {
+        const response = await fetch('./assets/hotel.json')
+        return await response.json() //returns the JSON ojbect hotel.json[hotel]
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+let hotelData = {}
+getHotelData().then(data => hotelData = data)
+
+//attach to click handlers on the anchor tags
+// document.querySelectorAll('a')
+let hotelList = document.querySelectorAll('a')
+hotelList.forEach(hotel => {
+    
+    let hotelAnchor = hotel.addEventListener('click', hotelInfo)
+    
+});
+
+
+
+
+
+
+function hotelInfo(event) {
+    let hotelChoice = hotelData.hotels.find(hotel => {
+        return event.target.id === hotel.name.toLowerCase()
+    })
+
+    console.log(hotelChoice)
+}
